a4f0c12793603baf25ce73dd701e23ec12c4e413
Elvita Kalvāne <elvita.kalvane@gmail.com>
2021-12-26 13:10:30 +0100

    add model specs, update request spec

--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -1,5 +1,9 @@
-require 'rails_helper'
-
 RSpec.describe Recipe, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to have_db_column :title }
+  it { is_expected.to have_db_column :ingredients }
+  it { is_expected.to have_db_column :instructions }
+
+  it 'is expected to have a valid Factory' do
+    expect(create(:recipe)).to be_valid
+  end
 end
--- spec/requests/recipes/index_spec.rb
+++ spec/requests/recipes/index_spec.rb
@@ -1,5 +1,6 @@
 describe 'GET /api/recipes' do
   subject { response }
+  let!(:recipe) { create(:recipe, title: 'I can override Factory values here') }
 
   before do
     get '/api/recipes'
@@ -9,7 +10,11 @@ describe 'GET /api/recipes' do
     expect(subject.status).to eq 200
   end
 
-  it 'is expected to respond with an empty array' do
-    expect(response_json).to eq []
+  it 'is expected to respond with a recipe' do
+    expect(response_json.size).to eq 1
+  end
+
+  it 'is expected to respond with a recipe' do
+    expect(response_json.first['title']).to eq 'I can override Factory values here'
   end
 end
