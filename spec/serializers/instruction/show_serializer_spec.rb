1fc9618043e9a1d5cd97a04a56729ac3a8bf1048
Elvita Kalvāne <elvita.kalvane@gmail.com>
2022-01-26 19:13:50 +0100

    add specs to check serializers

---
+++ spec/serializers/instruction/show_serializer_spec.rb
@@ -0,0 +1,21 @@
+RSpec.describe Instruction::ShowSerializer, type: :serializer do
+  let(:instruction) { create(:instruction) }
+
+  let(:serialization) do
+    ActiveModelSerializers::SerializableResource.new(instruction, serializer: described_class)
+  end
+
+  subject { JSON.parse(serialization.to_json) }
+
+  it 'is expected to wrap data in a key reflecting the resource name' do
+    expect(subject.keys).to match ['instruction']
+  end
+
+  it 'is expected to contain keys with specific data types' do
+    expect(subject).to match(
+      'instruction' => {
+        'instruction' => an_instance_of(String)
+      }
+    )
+  end
+end
