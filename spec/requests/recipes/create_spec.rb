759046b9bf639a5b51778e4b408d0379a91b91bf
Elvita Kalvāne <elvita.kalvane@gmail.com>
2022-01-30 15:38:32 +0100

    update spec to check that api returns only message not recipe data remove nestted attributes

--- spec/requests/recipes/create_spec.rb
+++ spec/requests/recipes/create_spec.rb
@@ -7,9 +7,9 @@ describe 'POST /api/recipes', type: :request do
            params: {
              recipe: {
                title: 'nom nom',
-               ingredients_attributes: [{  amount: 100, unit: 'grams', name: 'sugar' },
-                                        {  amount: 500, unit: 'grams', name: 'chocolate' }],
-               instructions_attributes: [{ instruction: 'mix together' }, { instruction: 'bake' }]
+               ingredients: [{  amount: 100, unit: 'grams', name: 'sugar' },
+                             { amount: 500, unit: 'grams', name: 'chocolate' }],
+               instructions: [{ instruction: 'mix together' }, { instruction: 'bake' }]
              }
            }
     end
@@ -25,17 +25,17 @@ describe 'POST /api/recipes', type: :request do
     end
 
     it 'is expected to return the new object with a title' do
-      expect(response_json['title']).to eq 'nom nom'
+      expect(response_json['message']).to eq 'Recipe was created successfully'
     end
 
-    it 'is expected to return the new object with recipe ingredients' do
-      expect(response_json['ingredients']).to eq [{ 'amount' => 100, 'name' => 'sugar', 'unit' => 'grams' },
-                                                  { 'amount' => 500, 'name' => 'chocolate', 'unit' => 'grams' }]
-    end
+    # it 'is expected to return the new object with recipe ingredients' do
+    #   expect(response_json['ingredients']).to eq [{ 'amount' => 100, 'name' => 'sugar', 'unit' => 'grams' },
+    #                                               { 'amount' => 500, 'name' => 'chocolate', 'unit' => 'grams' }]
+    # end
 
-    it 'is expected to return the new object with recipe ingredients' do
-      expect(response_json['instructions']).to eq [{ 'instruction' => 'mix together' }, { 'instruction' => 'bake' }]
-    end
+    # it 'is expected to return the new object with recipe ingredients' do
+    #   expect(response_json['instructions']).to eq [{ 'instruction' => 'mix together' }, { 'instruction' => 'bake' }]
+    # end
   end
 
   describe 'unsuccessfully' do
@@ -56,9 +56,9 @@ describe 'POST /api/recipes', type: :request do
         post '/api/recipes',
              params: {
                recipe: {
-                 ingredients_attributes: [{ amount: 100, unit: 'grams', name: 'sugar' },
-                                          { amount: 500, unit: 'grams', name: 'chocolate' }],
-                 instructions_attributes: [{ instruction: 'mix together' }, { instruction: 'bake' }]
+                 ingredients: [{ amount: 100, unit: 'grams', name: 'sugar' },
+                               { amount: 500, unit: 'grams', name: 'chocolate' }],
+                 instructions: [{ instruction: 'mix together' }, { instruction: 'bake' }]
                }
              }
       end
