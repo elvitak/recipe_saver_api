0525529bfd5a0313c6f7188a632b9fd014282a3f
Elvita Kalvāne <elvita.kalvane@gmail.com>
2022-03-24 08:32:00 +0100

    add spec for image attachement

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,5 +2,6 @@ class Recipe < ApplicationRecord
   validates_presence_of :title
   has_many :instructions
   has_many :ingredients
+  has_one_attached :image
   accepts_nested_attributes_for :instructions, :ingredients
 end
